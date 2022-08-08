a0d8af140c322f516d0fb11e9a5c6ccfd27ee9e8
Jamiu Abdsalami <jamiuabdsalami@outlook.com>
2022-08-07 17:25:51 -0700

    Fix category route

--- routes/categories.js
+++ routes/categories.js
@@ -1,1 +1,30 @@
+const router = require("express").Router();
+const Category = require("../models/Category");
+
+// Create new category
+router.post("/", async (req, res) => {
+
+    // Catch all information sent from body
+    const newCategory = new Category(req.body);
+    try {
+        const savedCategory = await newCategory.save();
+        res.status(200).json(savedCategory);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// New line
+
+// Fetch all categories
+// router.get("/", async (req, res) => {
+//     try {
+//       const categories = await Category.find();
+//       res.status(200).json(categories);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+
 module.exports = router;
